83a61d4ceef002adc6d4319e61255997d21766b5
Jose Fernandez-Castano <josefernandezcastano@gmail.com>
2019-03-20 16:47:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+
+const hq=42;
+
+function distanceFromHqInBlocks(blocks){
+return Math.abs(blocks-hq);
+}
+
+function distanceFromHqInFeet(feet){
+  return distanceFromHqInBlocks(feet)*264
+}
+
